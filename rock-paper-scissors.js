292c7e9cefdc4cc3b04cf62d2a825c9f72a314f4
Aaron_Proctor <119529793+mprocto8@users.noreply.github.com.>
2024-10-01 13:52:51 -0500

    Get computer choice and return string value
    Use random number generation to get random number
    between 0 and 99.
    
    Divide 0 through 99 into three equal parts for
    string selection. Have divided parts represent
    strings "Rock" "Paper" or "Scissors".

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,22 @@
 console.log("Javascript file successfully linked")
+
+function getRandomInt () {
+    return Math.floor(Math.random() * 100);
+}
+let num = getRandomInt();
+
+function getComputerChoice () {
+    if (num <= 33) {
+        console.log("Rock");
+    } else if (num >= 34 && num <= 66) {
+        console.log("Paper");
+    } else if (num >= 67) {
+        console.log("Scissors");
+    }
+}
+
+let cpuChoice = getComputerChoice();
+
+function getHumanChoice () {
+    prompt("Choose Rock, Paper, or Scissors by inputting 1, 2, or 3", "type 1, 2, or 3")
+}
