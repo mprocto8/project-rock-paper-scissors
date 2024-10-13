b2e04a79a6910b4cb6cda751ac99a56dcaa6b550
Aaron_Proctor <119529793+mprocto8@users.noreply.github.com.>
2024-10-13 14:35:27 -0500

    Create play game function and add notes to code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,6 @@
 console.log("Javascript file successfully linked")
 
+// Logic for getting computer choice
 function getComputerChoice () {
     const choices = ['rock', 'paper', 'scissors'];
     const randomChoice = Math.floor(Math.random() * choices.length);
@@ -7,6 +8,7 @@ function getComputerChoice () {
 }
 console.log(getComputerChoice());
 
+// Logic for getting human choice
 function getHumanChoice() {
     const humanChoice = prompt("Enter Rock, Paper, or Scissors",  "Rock, Paper, or Scissors");
     return humanChoice.toLowerCase();
@@ -17,34 +19,38 @@ console.log(getHumanChoice());
 let humanScore = 0;
 let computerScore = 0; 
 
+function playGame() {
+    // Keep track of score
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    //Normalize inputs
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
+    function playRound(humanChoice, computerChoice) {
+        // Normalize inputs
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
 
-    // Decide winner
-    if (humanChoice === computerChoice) {
-        console.log(`Tie! Both participants selected ${humanChoice}.`);
-    } else if (
-    // Human wins
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'rock')
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-        humanScore++;
-    } else if (
-        //Computer wins
-        (humanChoice === 'rock' && computerChoice === 'paper') ||
-        (humanChoice === 'scissors' && computerChoice === 'rock') ||
-        (humanChoice === 'paper' && computerChoice === 'scissors')
-    ) { 
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
-        computerScore++; 
+        // Decide winner
+        if (humanChoice === computerChoice) {
+            console.log(`Tie! Both participants selected ${humanChoice}.`);
+        } else if (
+        // Human wins
+            (humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper') ||
+            (humanChoice === 'paper' && computerChoice === 'rock')
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        } else if (
+        // Computer wins
+            (humanChoice === 'rock' && computerChoice === 'paper') ||
+            (humanChoice === 'scissors' && computerChoice === 'rock') ||
+            (humanChoice === 'paper' && computerChoice === 'scissors')
+        ) { 
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++; 
+        }
     }
 }
-
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
