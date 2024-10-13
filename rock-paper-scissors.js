7558a83e392f255f6eaf5300d8998660149a82b0
Aaron_Proctor <119529793+mprocto8@users.noreply.github.com.>
2024-10-13 12:29:00 -0500

    Simplify functions for computer and human choice
    Use an array to get computer choice and plain text
    to get human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,61 +1,53 @@
 console.log("Javascript file successfully linked")
 
-function getRandomInt (max) {
-    return Math.floor(Math.random() * max);
-}
-let num = getRandomInt(3);
-
 function getComputerChoice () {
-    if (num == 0) {
-        return "Rock";
-    } else if (num == 1) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
 }
-
 console.log(getComputerChoice());
 
-const humanChoice = prompt("Choose Rock, Paper, or Scissors by inputting 1, 2, or 3",  "type 1, 2, or 3")
-
 function getHumanChoice () {
-    if (humanChoice == 1) {
-        return "Rock";
-    } else if (humanChoice == 2) {
-        return "Paper";
-    } else {
-        return "Scissors";
+    let humanChoice = prompt("Enter Rock, Paper, or Scissors",  "Rock, Paper, or Scissors");
+    return(humanChoice);    
     }
-}
 
 console.log(getHumanChoice());
 
-var humanScore = 0;
-var computerScore = 0;
+   let humanScore = 0;
+   let computerScore = 0; 
+
 
 function playRound(humanChoice, computerChoice) {
+    //Normalize inputs
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice
     // Logic for Rock as human choice
-    if (humanChoice == 1 && computerChoice == 0) {
+    if (humanChoice == "1" && computerChoice == 0) {
         console.log("Draw! Rock ties with Rock");
     } else if (humanChoice == 1 && computerChoice == 1) {
-        computerScore++
+        gameScore.computerScore++;
         console.log("You lose! Paper beats Rock");
     } else if (humanChoice == 1 && computerChoice == 2) {
+        gameScore.humanScore++;
         console.log("You win! Rock beats Scissors");  
     }
     // Logic for Paper as human choice
     else if (humanChoice == 2 && computerChoice == 0) {
+        gameScore.humanScore++;
         console.log("You win! Paper beats Rock");
     } else if (humanChoice == 2 && computerChoice == 1) {
         console.log("Draw! Paper ties with Paper");
     } else if (humanChoice == 2 && computerChoice == 2) {
+        gameScore.computerScore++;
         console.log("You lose! Scissors beats Paper");
     } 
     // Logic for Scissors as human choice
     else if (humanChoice == 3 && computerChoice == 0) {
+        gameScore.computerScore++;
         console.log("You lose! Rock beats Scissors");
     } else if (humanChoice == 3 && computerChoice == 1) {
+        gameScore.humanScore++;
         console.log("You win! Scissors beats Paper");
     } else if (humanChoice == 2 && computerChoice == 0) {
         console.log("Draw! Scissors ties with Scissors");
